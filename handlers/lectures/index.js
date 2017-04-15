ad2fc8068d8dfe6f4287123ecfd5f7a6766444ec
Artem Maksimov <artem@getcodified.com>
2017-04-15 22:37:12 +0300

    Link lecture and teacher by id, add validation for existing user

--- handlers/lectures/index.js
+++ handlers/lectures/index.js
@@ -3,7 +3,7 @@ module.exports = function(app, db) {
     app.get('/schedule', function (req, res) {
         const schedule = db.get('schedule').value();
         schedule.map(function (lecture) {
-            const teacher = db.get('teachers').find({'name': lecture.teacher}).value();
+            const teacher = db.get('teachers').find({'id': lecture.teacher}).value();
             lecture.teacherData = teacher;
         });
         res.render('lectures/schedule', {'schedule': schedule});
@@ -16,6 +16,15 @@ module.exports = function(app, db) {
 
     // Обработка добавления лекции
     app.post('/new_lecture', function (req, res) {
+        const teacher = db.get('teachers').find({ name : req.body.teacher }).value();
+        if(!teacher) {
+            res.render('lectures/new_lecture', { error : 'Лектора с именем ' + req.body.teacher + ' не существует в базе' });
+            return;
+        }
+
+        // Замена имени лектора на id
+        req.body.teacher = teacher.id;
+
         db.get('schedule').insert(req.body).write().then(function (result) {
             res.redirect('/schedule/' + result.id);
         });
@@ -27,11 +36,26 @@ module.exports = function(app, db) {
             .find({id: req.params.id})
             .value();
 
+        const teacher = db.get('teachers')
+            .find({'id': lecture.teacher})
+            .value();
+
+        lecture.teacherData = teacher;
+
         res.render('lectures/lecture', {'lecture': lecture});
     });
 
     // Обновить экземпляр лекции по id
     app.post('/schedule/:id', function (req, res) {
+        const teacher = db.get('teachers').find({ name : req.body.teacher }).value();
+        if(!teacher) {
+            res.render('lectures/lecture', { error : 'Лектора с именем ' + req.body.teacher + ' не существует в базе', lecture : req.body });
+            return;
+        }
+
+        // Замена имени лектора на id
+        req.body.teacher = teacher.id;
+
         db.get('schedule').find({id: req.params.id}).assign(req.body).write().then(function (result) {
             res.redirect('/schedule/' + result.id);
         });
